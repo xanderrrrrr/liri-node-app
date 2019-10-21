f8ba65f7601e1422d11c65e0fbf6fde51711b568
Xander C <xander.canedo@gmail.com>
2019-10-21 10:31:20 -0600

    init

---
+++ liri.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+// var spotify = new Spotify(keys.spotify);
+
+console.log(process.env.SPOTIFY_ID) 
+console.log(process.env.SPOTIFY_SECRET)
